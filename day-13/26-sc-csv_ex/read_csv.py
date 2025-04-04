e30bfc7a9e0ee3d63b419e37e07978a62cd00b05
mindful-ai <mindfullearningindia@outlook.com>
2025-04-04 10:01:51 +0530

    Add files via upload

---
+++ day-13/26-sc-csv_ex/read_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('data.csv','r') as f:
+  data = csv.reader(f)
+  print('DATA TYPE: ', type(data))
+  for row in data:
+        print(row)
+
+
+reader = csv.DictReader(open("data.csv"))
+for raw in reader:
+    print(dict(raw))
